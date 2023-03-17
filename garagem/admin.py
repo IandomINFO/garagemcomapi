849fe31761a7d8b2efc627b80e82edd95c79a157
ian <ianrobertoestudos10@gmail.com>
2023-03-17 11:03:57 -0300

    garagem com api(naofizainda)

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Marca, Categoria
+from .models import Marca, Categoria, Acessorio, Cor, Veiculo
 
 admin.site.register(Marca)
 admin.site.register(Categoria)
+admin.site.register(Veiculo)
+admin.site.register(Cor)
+admin.site.register(Acessorio)
--- garagem/models.py
+++ garagem/models.py
@@ -16,3 +16,31 @@ class Categoria(models.Model):
 
     def __str__(self):
         return self.descricao
+
+
+class Acessorio(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+
+
+class Cor(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+
+
+class Veiculo(models.Model):
+    nome = models.CharField(max_length=50)
+    ano = models.IntegerField(default=0, null=True, blank=True)
+    preco = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="veiculos")
+    categoria = models.ForeignKey(
+        Categoria, on_delete=models.PROTECT, related_name="veiculos"
+    )
+    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculos")
+
+    def __str__(self):
+        return self.nome
